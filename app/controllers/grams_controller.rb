479d75ddf66b30e66a47af26da9b33caa9228bcc
Joey <joey.s.hornsby@gmail.com>
2021-03-29 10:16:05 +0100

    edit gram descriptions

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,16 +5,32 @@ class GramsController < ApplicationController
     @comment = Comment.new
   end
 
+  def edit
+    @gram = Gram.find(params[:id])
+  end
+
   def new
     @gram = Gram.new
   end
 
   def create
     @user = current_user
-    @gram = @user.grams.create(gram_params)
+    @gram = @user.grams.create(create_gram_params)
     redirect_to grams_url
   end
 
+  def update
+    @gram = Gram.find(params[:id])
+    respond_to do |format|
+      if @gram.update(edit_gram_params)
+        format.html { redirect_to :root, notice: "Gram was successfully updated." }
+        format.json { render :show, status: :ok, location: @gram }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @gram.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   def destroy
     @gram = Gram.find(params[:id])
     @gram.destroy
@@ -25,7 +41,11 @@ class GramsController < ApplicationController
   end
 
   private
-  def gram_params
-    params.require(:gram).permit(:title, :image, :description)
+  def create_gram_params
+    params.require(:gram).permit(:image, :description)
+  end
+
+  def edit_gram_params
+    params.require(:gram).permit(:description)
   end
 end
