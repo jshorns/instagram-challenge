6b16bcf3fddca4f4ad7693300f418baebee0bb05
Joey <joey.s.hornsby@gmail.com>
2021-03-29 12:03:36 +0100

    navbar

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+import { environment } from '@rails/webpacker'
+import { ProvidePlugin } from 'webpack'
+environment.plugins.append( 'Provide',
+new ProvidePlugin({
+    s: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+})
+)
+
+export default environment
+
 module.exports = environment
